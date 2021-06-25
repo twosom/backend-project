d95436ff2eed306129d74240f110188337f8a34d
desirel <two_somang@icloud.com>
2021-06-25 21:08:46 +0900

    feat: 댓글 조회 기능 추가

--- src/main/java/com/daib/backend/dto/CommentViewDto.java
+++ src/main/java/com/daib/backend/dto/CommentViewDto.java
@@ -6,25 +6,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class PostViewDto {
-
+public class CommentViewDto {
 
     private Long id;
-
-    private String title;
-
-    private String content;
-
     private String writer;
-
+    private String content;
     private LocalDateTime createdAt;
 
-    List<CommentViewDto> commentList = new ArrayList<>();
 }
