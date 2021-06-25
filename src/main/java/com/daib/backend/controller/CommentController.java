fd485d16df82af257f42ba019f81a7c5f14ce376
desirel <two_somang@icloud.com>
2021-06-25 21:31:34 +0900

    feat: 댓글 수정 기능 추가

--- src/main/java/com/daib/backend/controller/CommentController.java
+++ src/main/java/com/daib/backend/controller/CommentController.java
@@ -1,10 +1,13 @@
 package com.daib.backend.controller;
 
+import com.daib.backend.domain.board.Comment;
 import com.daib.backend.domain.board.Post;
 import com.daib.backend.dto.PostViewDto;
+import com.daib.backend.form.CommentEditForm;
 import com.daib.backend.form.CommentForm;
 import com.daib.backend.repository.PostRepository;
 import com.daib.backend.service.CommentService;
+import com.daib.backend.validator.comment.CommentEditFormValidator;
 import com.daib.backend.validator.comment.CommentFormValidator;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -12,7 +15,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -22,15 +27,23 @@ import javax.validation.Valid;
 public class CommentController {
 
     private final CommentService commentService;
+
     private final CommentFormValidator commentFormValidator;
+    private final CommentEditFormValidator commentEditFormValidator;
 
     private final PostRepository postRepository;
     private final ModelMapper modelMapper;
 
 
+
     @InitBinder("commentForm")
     void commentFormInitBinder(WebDataBinder webDataBinder) {
         webDataBinder.addValidators(commentFormValidator);
+    }
+
+    @InitBinder("commentEditForm")
+    void commentEditFormInitBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(commentEditFormValidator);
     }
 
     @PostMapping("/comment/new")
@@ -44,4 +57,23 @@ public class CommentController {
         commentService.createNewComment(commentForm);
         return "redirect:/post/" + commentForm.getPostId();
     }
+
+    @GetMapping("/comment/edit/{id}")
+    public String editCommentForm(@PathVariable("id") Comment comment, Model model) {
+        //TODO 해당 comment 없으면 예외처리
+        model.addAttribute(modelMapper.map(comment, CommentEditForm.class));
+        return "comment/edit-comment";
+    }
+
+    @PostMapping("/comment/edit/{id}")
+    public String editComment(@PathVariable("id") Long id, @Valid CommentEditForm commentEditForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "comment/edit-comment";
+        }
+
+        Long postId = commentService.editComment(commentEditForm, id);
+        return "redirect:/post/" + postId;
+    }
+
+
 }
--- src/main/java/com/daib/backend/service/CommentService.java
+++ src/main/java/com/daib/backend/service/CommentService.java
@@ -2,13 +2,16 @@ package com.daib.backend.service;
 
 import com.daib.backend.domain.board.Comment;
 import com.daib.backend.domain.board.Post;
+import com.daib.backend.form.CommentEditForm;
 import com.daib.backend.form.CommentForm;
 import com.daib.backend.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class CommentService {
 
@@ -21,5 +24,11 @@ public class CommentService {
         post.setId(commentForm.getPostId());
         comment.setPost(post);
         commentRepository.save(comment);
+    }
+
+    public Long editComment(CommentEditForm commentEditForm, Long id) {
+        Comment comment = commentRepository.findById(id).get();
+        modelMapper.map(commentEditForm, comment);
+        return comment.getPost().getId();
     }
 }
