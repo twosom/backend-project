b56b22bf5ec028e2e811ecc89ce480148908d522
desirel <two_somang@icloud.com>
2021-06-25 20:28:39 +0900

    feat: 댓글 저장 기능 추가

--- src/main/java/com/daib/backend/form/PostEditForm.java
+++ src/main/java/com/daib/backend/form/PostEditForm.java
@@ -5,16 +5,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class PostEditForm {
 
+
     private Long id;
+
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String content;
+
+    @NotBlank
     private String writer;
+
+    @NotBlank
     private String password;
 
 }
