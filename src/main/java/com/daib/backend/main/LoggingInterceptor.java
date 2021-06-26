f6b599a6e523e926e83d1554a19011a647dfa818
desirel <two_somang@icloud.com>
2021-06-26 11:16:38 +0900

    feat: 모든 restful api 에 대한 로깅 처리

---
+++ src/main/java/com/daib/backend/main/LoggingInterceptor.java
@@ -0,0 +1,29 @@
+package com.daib.backend.main;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        final ContentCachingRequestWrapper cachingRequest = (ContentCachingRequestWrapper) request;
+        final ContentCachingResponseWrapper cachingResponse = (ContentCachingResponseWrapper) response;
+        log.info("RequestBody : {} / ResponseBody : {}", objectMapper.readTree(cachingRequest.getContentAsByteArray()), objectMapper.readTree(cachingResponse.getContentAsByteArray()));
+
+
+    }
+}
--- src/main/java/com/daib/backend/post/controller/PostApiController.java
+++ src/main/java/com/daib/backend/post/controller/PostApiController.java
@@ -8,6 +8,8 @@ import com.daib.backend.post.form.PostForm;
 import com.daib.backend.post.repository.PostQueryRepository;
 import com.daib.backend.post.service.PostService;
 import com.daib.backend.post.validator.PostEditFormValidator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -31,6 +33,8 @@ public class PostApiController {
     private final PostService postService;
     private final PostEditFormValidator postEditFormValidator;
 
+    private final ObjectMapper objectMapper;
+
     @InitBinder("postEditForm")
     public void postEditFormInitBinder(WebDataBinder webDataBinder) {
         webDataBinder.addValidators(postEditFormValidator);
@@ -49,7 +53,7 @@ public class PostApiController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity viewPost(@PathVariable("id") Long id) {
+    public ResponseEntity viewPost(@PathVariable("id") Long id) throws JsonProcessingException {
         try {
             PostViewDto postViewDto = postService.getPost(id);
 
@@ -60,12 +64,12 @@ public class PostApiController {
         } catch (PostNotFoundException e) {
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
-                    .body(e.getMessage());
+                    .body(objectMapper.writeValueAsString(e.getMessage()));
         }
     }
 
     @PostMapping("/new")
-    public ResponseEntity createNewPost(@RequestBody @Valid PostForm postForm, Errors errors) {
+    public ResponseEntity createNewPost(@RequestBody @Valid PostForm postForm, Errors errors) throws JsonProcessingException {
         if (errors.hasErrors()) {
 
             List<String> errorMessages = getErrorMessages(errors);
@@ -76,9 +80,10 @@ public class PostApiController {
         }
 
         Long postId = postService.createNewPost(postForm);
+        String message = postId + "번 ID 의 게시글이 생성되었습니다.";
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(postId + "번 ID 의 게시글이 생성되었습니다.");
+                .body(objectMapper.writeValueAsString(message));
     }
 
     private List<String> getErrorMessages(Errors errors) {
@@ -90,7 +95,7 @@ public class PostApiController {
 
     @PostMapping("/edit")
     public ResponseEntity editPost(@Valid @RequestBody PostEditForm postEditForm,
-                                   Errors errors) {
+                                   Errors errors) throws JsonProcessingException {
 
         if (errors.hasErrors()) {
             List<String> errorMessages = getErrorMessages(errors);
@@ -102,27 +107,29 @@ public class PostApiController {
 
         try {
             postService.editPost(postEditForm);
+            String message = postEditForm.getId() + "에 해당하는 게시글의 수정이 완료되었습니다.";
             return ResponseEntity
                     .status(HttpStatus.OK)
-                    .body(postEditForm.getId() + "에 해당하는 게시글의 수정이 완료되었습니다.");
+                    .body(objectMapper.writeValueAsString(message));
         } catch (PostNotFoundException e) {
             return ResponseEntity
                     .badRequest()
-                    .body(e.getMessage());
+                    .body(objectMapper.writeValueAsString(e.getMessage()));
         }
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity deletePost(@PathVariable("id") Long id) {
+    public ResponseEntity deletePost(@PathVariable("id") Long id) throws JsonProcessingException {
         try {
             postService.removePost(id);
+            String message = id + "에 해당하는 게시글의 삭제가 완료되었습니다.";
             return ResponseEntity
                     .status(HttpStatus.OK)
-                    .body(id + "에 해당하는 게시글의 삭제가 완료되었습니다.");
+                    .body(objectMapper.writeValueAsString(message));
         } catch (PostNotFoundException e) {
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
-                    .body(e.getMessage());
+                    .body(objectMapper.writeValueAsString(e.getMessage()));
         }
     }
 }
